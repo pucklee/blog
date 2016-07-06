287188f01e87e3218beab0c25b81930d7bf4efde
pucklee <497003812@qq.com>
2016-07-06 18:48:48 +0800

    拆分文件

--- api/blog/blog.sql.design.js
+++ api/blog/blog.sql.design.js
@@ -3,11 +3,11 @@
   // 文章表 article
   	article_id     	:     INT,            // 文章id
   	article_title  	:     VARCHAR,		  // 文章标题	
-  	article_type	:     TINYINT,		  // 0为转载，1为原创
-  	publish_time	: 	  DATETIME,		  // 发表时间
-  	updata_time		: 	  DATETIME,		  // 修改时间
-  	author			: 	  VARCHAR,		  // 作者(原创人)
-  	user_id			: 	  INT,		  	  // 转载人(编辑人)
+  	article_type	  :     TINYINT,		  // 0为转载，1为原创
+  	publish_time	  : 	  DATETIME,		  // 发表时间
+  	updata_time		  : 	  DATETIME,		  // 修改时间
+  	author			    : 	  VARCHAR,		  // 作者(原创人)
+  	user_id			    : 	  INT,		  	  // 转载人(编辑人)
   	tag_id          :     INT,            // 标签id
 
 
@@ -19,15 +19,15 @@
   // 标签表 article_tag
   	tag_id          :     INT,            // 标签id
   	tag_name        :     VARCHAR,        // 标签name
-  	tag_count		:     INT,			  // 标签数量
-  	add_time		:     DATETIME,			  // 标签创建时间
+  	tag_count		    :     INT,			  // 标签数量
+  	add_time		    :     DATETIME,			  // 标签创建时间
 
 
   // 用户表 user
   	user_id        	:     INT,            // 用户id
   	user_name       :     VARCHAR,        // 用户name
   	password      	:     VARCHAR,        // 用户密码
-  	email      		:     VARCHAR,        // 用户密码
+  	email      		  :     VARCHAR,        // 用户密码
   	user_type       :     TINYINT,    	  // 用户权限，0为游客，1为管理员
 
 
